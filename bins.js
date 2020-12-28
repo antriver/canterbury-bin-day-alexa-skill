faeaab28aa96b7e997f679e74653a4b3b425f953
Anthony Kuske <anthonykuske@gmail.com>
2020-12-28 21:52:06 +0000

    Initial commit of existing skill files

---
+++ bins.js
@@ -0,0 +1,116 @@
+const request = require('request');
+const moment = require('moment-timezone');
+const { parse } = require('node-html-parser');
+
+const getRound = (postcode, addressLine1) => {
+    return new Promise((resolve, reject) => {
+        let lowerAddress = addressLine1.toLowerCase();
+        request(`https://www.canterbury.gov.uk/homepage/29/find_your_bin_collection_dates?postcode=` + encodeURIComponent(postcode), (err, res, body) => {
+            if (err || !body) {
+                console.error(err);
+                reject(new Error('I couldn\'t load your collection round from the council website.'));
+                return;
+            }
+
+            // console.log('find_your_bin_collection_dates response', body);
+
+            let doc = parse(body);
+            let chooser = doc.querySelector('#chooseaddress');
+
+            if (!chooser) {
+                reject(new Error('I couldn\'t find any addresses for your post code. Please check you have entered your post code correctly and it is within Canterbury.'));
+                return;
+            }
+
+            let options = chooser.querySelectorAll('option');
+            for (let i in options) {
+                if (options.hasOwnProperty(i)) {
+                    let o = options[i];
+                    let value = o.attributes.value;
+                    let round = value.replace('https://www.canterbury.gov.uk/bincalendar?round=', '');
+                    let address = o.innerHTML.trim();
+                    // console.log(address, round);
+                    if (address.toLowerCase().indexOf(lowerAddress) !== -1) {
+                        resolve(round);
+                    }
+                }
+            }
+
+            reject(new Error('I couldn\'t find your address for that postcode. Please check how you entered your address in the Alexa app matches how it appears on the council website.'));
+        });
+    });
+};
+
+const getNextCollection = (round) => {
+    return new Promise((resolve, reject) => {
+        request(`https://www.canterbury.gov.uk/bincalendar?round=${round}`, (err, res, body) => {
+            if (err || !body) {
+                console.error(err);
+                reject(new Error('I couldn\'t load your collection date from the council website.'));
+                return;
+            }
+
+            // console.log('bincalendar response', body);
+
+            let doc = parse(body);
+
+            let answer = doc.querySelector('.widget--your-collection-day').querySelectorAll('p')[0];
+
+            // console.log('answer', answer);
+
+            resolve(formatCollectionText(answer));
+        });
+    });
+};
+
+/**
+ * @param {HTMLElement} collectionEl
+ */
+const formatCollectionText = (collectionEl) => {
+    // console.log('collectionEl', collectionEl.innerHTML);
+
+    let type = collectionEl.querySelector('strong').innerHTML.trim();
+    let speakType = type === 'general' ? 'general waste' : type;
+    // console.log('type', type);
+
+    let dateString = collectionEl.querySelector('.large').innerHTML.trim();
+    // console.log('dateString', dateString);
+
+    moment.tz.guess();
+    // console.log('now', (new Date()).toString());
+    // console.log('now london', moment.tz('Europe/London').toString());
+
+    let date = moment.tz(dateString, 'dddd D MMMM', 'Europe/London');
+    // console.log('date', date.toString());
+
+    let now = moment.tz('Europe/London');
+    const isToday = date.format('Y-MM-DD') === now.format('Y-MM-DD');
+    // console.log('isToday', isToday);
+
+    let tomorrow = moment.tz('Europe/London').add(1, 'd');
+    const isTomorrow = date.format('Y-MM-DD') === tomorrow.format('Y-MM-DD');
+    // console.log('isTomorrow', isTomorrow);
+
+    let lines = collectionEl.innerHTML.split('<br />');
+    let lastLine = lines[lines.length - 1];
+    lastLine = lastLine.replace('Includes', 'This includes');
+    // console.log('lastLine', lastLine);
+
+    let formattedDate = date.format('dddd MMMM Do');
+    let collectedOn;
+    if (isToday) {
+        collectedOn = `today (${formattedDate})`;
+    } else if (isTomorrow) {
+        collectedOn = `tomorrow (${formattedDate})`;
+    } else {
+        collectedOn = `on ${formattedDate}`;
+    }
+
+    // Build the text.
+    return `Your next collection is ${speakType}, ${collectedOn}. ${lastLine}`;
+};
+
+module.exports = {
+    getRound,
+    getNextCollection
+};
---
+++ index.js
@@ -0,0 +1,110 @@
+// This sample demonstrates handling intents from an Alexa skill using the Alexa Skills Kit SDK (v2).
+// Please visit https://alexa.design/cookbook for additional examples on implementing slots, dialog management,
+// session persistence, api calls, and more.
+const Alexa = require('ask-sdk-core');
+const { getRound, getNextCollection } = require('./bins');
+
+const RubbishIntentHandler = {
+    canHandle(handlerInput) {
+        return (Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'BIntent')
+            || Alexa.getRequestType(handlerInput.requestEnvelope) === 'LaunchRequest';
+    },
+    async handle(handlerInput) {
+
+        const { requestEnvelope, serviceClientFactory, responseBuilder } = handlerInput;
+        const consentToken = requestEnvelope.context.System.user.permissions
+            && requestEnvelope.context.System.user.permissions.consentToken;
+        if (!consentToken) {
+            return responseBuilder
+                .speak('Please enable Location permissions in the Amazon Alexa app.')
+                .withAskForPermissionsConsentCard(['read::alexa:device:all:address'])
+                .getResponse();
+        }
+
+        try {
+            const { deviceId } = requestEnvelope.context.System.device;
+            const deviceAddressServiceClient = serviceClientFactory.getDeviceAddressServiceClient();
+            const address = await deviceAddressServiceClient.getFullAddress(deviceId);
+
+            // console.log('Address successfully retrieved, now responding to user.', address);
+
+            let response;
+            if (address.addressLine1 === null && address.stateOrRegion === null) {
+                response = responseBuilder
+                    .speak(`It looks like you don't have an address set. You can set your address from the companion app.`)
+                    .getResponse();
+            } else {
+                try {
+                    // Got the address. Now find which bin round they are from CCC.
+                    const round = await getRound(address.postalCode, address.addressLine1);
+
+                    // Find the next collection from CCC.
+                    const nextCollection = await getNextCollection(round);
+
+                    return responseBuilder
+                        .speak(nextCollection)
+                        .getResponse();
+
+                } catch (e) {
+                    return responseBuilder
+                        .speak('Sorry, ' + e.message)
+                        .getResponse();
+                }
+            }
+            return response;
+        } catch (error) {
+            if (error.name !== 'ServiceError') {
+                const response = responseBuilder
+                    .speak('Uh Oh. Looks like something went wrong.')
+                    .getResponse();
+
+                return response;
+            }
+            throw error;
+        }
+    }
+};
+const SessionEndedRequestHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'SessionEndedRequest';
+    },
+    handle(handlerInput) {
+        // Any cleanup logic goes here.
+        return handlerInput.responseBuilder.getResponse();
+    }
+};
+
+// Generic error handling to capture any syntax or routing errors. If you receive an error
+// stating the request handler chain is not found, you have not implemented a handler for
+// the intent being invoked or included it in the skill builder below.
+const ErrorHandler = {
+    canHandle() {
+        return true;
+    },
+    handle(handlerInput, error) {
+        console.log(`~~~~ Error handled: ${error.stack}`);
+        const speakOutput = `Sorry, I had trouble doing what you asked. Please try again.`;
+
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .reprompt(speakOutput)
+            .getResponse();
+    }
+};
+
+// The SkillBuilder acts as the entry point for your skill, routing all request and response
+// payloads to the handlers above. Make sure any new handlers or interceptors you've
+// defined are included below. The order matters - they're processed top to bottom.
+exports.handler = Alexa.SkillBuilders.custom()
+    .withApiClient(
+        new Alexa.DefaultApiClient()
+    )
+    .addRequestHandlers(
+        RubbishIntentHandler,
+        SessionEndedRequestHandler
+    )
+    .addErrorHandlers(
+        ErrorHandler
+    )
+    .lambda();
