13138f09ce8d6dfb79204c6de6fb7f389abfe830
Anthony Kuske <anthonykuske@gmail.com>
2022-06-22 12:44:19 +0100

    Fix spelling

--- bins.js
+++ bins.js
@@ -146,7 +146,7 @@ const collectionToString = (collection) => {
 const convertCollectionName = (collectionName) => {
     const names = {
         'blackBinDay': 'general waste',
-        'recyclingBinDay': 'recylcing',
+        'recyclingBinDay': 'recycling',
         'gardenBinDay': 'garden waste',
         'foodBinDay': 'food waste',
     }
